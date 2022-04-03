05895f1d4d2292d4aa17f881fda1ba2eadfae3b4
Roark23 <roarkleesegler@gmail.com>
2022-04-03 13:48:20 -0500

    Challenge Completed

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -22,4 +22,64 @@ last_name,
 title
 INTO unique_titles
 FROM retirement_titles
+WHERE (to_date = '9999-01-01')
 ORDER BY emp_no, title DESC;
+
+select * from unique_titles;
+
+
+-- The number of employees by their most recent job title who are about to retire.
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
+
+select * from retiring_titles;
+
+--Deliverable 2
+
+-- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+select * from mentorship_eligibilty;
+
+--Positions open for Staff and Departament: 
+SELECT DISTINCT ON (rt.emp_no) 
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title,
+	d.dept_name
+INTO unique_titles_dept
+FROM retirement_titles as rt
+INNER JOIN dept_emp as de
+ON (rt.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (d.dept_no = de.dept_no)
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+select * from unique_titles_dept
+
+-- How many positions need to be filled by title and department?
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO positions_open
+FROM (SELECT title, dept_name from unique_titles_dept) as ut
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
+
+select * from positions_open
